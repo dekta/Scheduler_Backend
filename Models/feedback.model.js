307434466dff9badab6fc5a4919b27b0b2f63b5f
EktaDhawal <51295793+dekta@users.noreply.github.com>
2023-04-10 19:46:31 +0530

    some update

--- Models/feedback.model.js
+++ Models/feedback.model.js
@@ -3,11 +3,9 @@ const mongoose = require('mongoose');
 const feedbackSchema = mongoose.Schema({
   name: {
     type: String,
-    required: true
   },
   email: {
     type: String,
-    required: true
   },
   message: {
     type: String,
